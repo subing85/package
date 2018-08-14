635b7327cab1218f6b46d66876c0df7e9659bdbe
unknown <subing85@gmail.com>
2018-08-15 02:49:12 +0800

    Studio Player 2

--- utils/studioPlayer.py
+++ utils/studioPlayer.py
@@ -3,27 +3,19 @@ Studio Player v0.1
 Date: August 12, 2018
 Last modified: August 12, 2018
 Author: Subin. Gopi(subing85@gmail.com)
-
 # Copyright(c) 2018, Subin Gopi
 # All rights reserved.
-
 # WARNING! All changes made in this file will be lost!
-
 Description
     This module contain studio player.
 '''
 
-
-import os
 import sys
-import json
+import os
 import warnings
-import copy
 import threading
-import re
 import time
 
-from pprint import pprint
 from functools import partial
 
 from PyQt4 import QtGui
@@ -44,45 +36,45 @@ FROM, BASE = uic.loadUiType(UI_FILE)
 
 class Player(FROM, BASE):
          
-    def __init__(self, *args):
-        super(Player, self).__init__(*args)
-        uic.loadUi(UI_FILE, self)    
-
-        try:
-            __file__
-        except NameError:
-            __file__ = sys.argv[0]
+    def __init__(self, **kwargs):        
+        super(Player, self).__init__()
+        uic.loadUi(UI_FILE, self) 
+          
+        self.footagePath = None
+        self.extension = 'jpg'
+        self.resolution = [422, 237]        
+        
+        if 'path' in kwargs:
+            self.footagePath = kwargs['path']        
+        if 'extension' in kwargs:
+            self.extension = kwargs['extension']
+            
+        if 'resolution' in kwargs:
+            self.resolution = kwargs['resolution'] 
+     
+        self.mediaObjects = True
+        self.fps = 25.00          
      
         style = studioStylesheet.Stylesheet(self) # set the ui style sheet
         style.setStylesheet()
         self.qtd = studioQtdress.QtDress(None)
         
-        self.resolution = [422, 237]
-        #self.mediaObject = 'play'
-        
-        self.footagePath = 'E:/Temp/old/Gallary_01'
-        self.fileList = self.getFiles()
-        
-        self.slider.setMinimum(1)   
-        self.spinBox.setMinimum(1)        
-        self.slider.setMaximum(len(self.fileList))
-        self.spinBox.setMaximum(len(self.fileList))
-        self.button_preview.setToolTip ('play')
-          
+        self.imageList = self.getFiles()
         self.defaultUiSettings()
         self.setIconAllWidgets()
         
         self.action_backword.triggered.connect(self.backword)
         self.action_forward.triggered.connect(self.forward)
         self.action_play.triggered.connect(self.playAndPause)
-        #self.action_stop.triggered.connect(self.stop)
+        self.action_stop.triggered.connect(self.stop)
         self.slider.valueChanged.connect(self.sliderPlay)
                 
     def defaultUiSettings(self):        
         self.setWindowTitle('Studio PLAYer v0.1')
         self.setWindowIcon(QtGui.QIcon(os.path.join(ICON_PATH, 'player.png')))
-        self.resize(QtCore.QSize(424, 275))
-                               
+        self.resize(QtCore.QSize(432, 324))
+        self.slider.setMinimum(1)   
+        self.slider.setMaximum(len(self.imageList))                               
         style = studioStylesheet.Stylesheet(self)
         style.setStylesheet()                        
         self.qtd.setToolBar(None,
@@ -100,104 +92,102 @@ class Player(FROM, BASE):
             self.qtd.qwidget = eachWidget         
             self.qtd.setIcon(ICON_PATH, width=100, height=25, lock=False)
 
-    def forward (self) :
-        currentFrame = self.slider.value ()
-        self.slider.setProperty ('value', currentFrame+1)  
+    def forward(self):
+        currentFrame = self.slider.value()
+        self.slider.setProperty('value', currentFrame+1)
                   
-    def backword (self) :
-        currentFrame = self.slider.value ()
-        self.slider.setProperty ('value', currentFrame-1)              
+    def backword(self):
+        currentFrame = self.slider.value()
+        self.slider.setProperty('value', currentFrame-1)              
             
-    def sliderPlay (self) :        
-        #self.playAndPauseAction('play')
-        currentFrame     = self.slider.value ()
-        self.playSequence([self.fileList[currentFrame]], 0)
-        
-    def playAndPauseAction(self, mode):
-        currentFrame = self.slider.value () 
-        currentFile = self.fileList[currentFrame-1]
-        self.button_preview.setIcon (QtGui.QIcon (os.path.join(self.footagePath, currentFile)))
-        self.button_preview.setIconSize (QtCore.QSize(self.resolution[0], self.resolution[1]))
-        self.button_preview.setMinimumSize (QtCore.QSize(self.resolution[0]+2, self.resolution[1]+2))             
-        self.spinBox.setValue(currentFrame)        
-        self.button_preview.setToolTip (mode)
-        
-    def playAndPause(self):
-        currentFrame     = self.slider.value ()    
-        endFrame     = self.slider.maximum ()
-    
-        if currentFrame==endFrame :
-            self.slider.setProperty ('value', 1)    
-            
-        mediaObject     = self.button_preview.toolTip ()
-    
-        if mediaObject=='play':
-            self.daemon     = True
-            self.pause     = False
-            self.state     = threading.Condition ()
-            
-            self.palyThread = threading.Thread (target=self.startPlay, args=())
-            self.palyThread.daemon = True
-            self.palyThread.start ()
-            
-            self.playAndPauseAction ('pause')
-        
-        if mediaObject=='pause':
-            with self.state :
-                self.pause     = True
-            
-            self.playAndPauseAction ('play')                       
+    def sliderPlay(self):        
+        currentFrame = self.slider.value()
+        if not self.imageList:
+            return
+        self.playSequence([self.imageList[currentFrame]], 0)
 
+    def playAndPause(self):
+        if not self.imageList:
+            return
+        currentFrame = self.slider.value()    
+        endFrame = self.slider.maximum()
         
-    def startPlay (self) :
-        startFrame     = self.slider.value ()    
-        endFrame     = self.slider.maximum ()
-        fps = 25
-        for loop in range (startFrame, endFrame, 1) :
-            with self.state :
-                if self.pause :
-                    self.state.wait ()
+        if currentFrame==endFrame:
+            self.slider.setProperty('value', 1)    
+            
+        mediaObject = self.mediaObjects
+        if self.mediaObjects:
+            self.daemon = True
+            self.pause = False
+            self.state = threading.Condition()
+            self.palyThread = threading.Thread(target=self.startPlay, args=())
+            self.palyThread.daemon = True
+            self.palyThread.start()
+            self.mediaObjects = False
+            icon = QtGui.QIcon ()
+            icon.addPixmap (QtGui.QPixmap (os.path.join(ICON_PATH, 'pause.png')), 
+                            QtGui.QIcon.Normal, 
+                            QtGui.QIcon.Off)                   
+            self.action_play.setIcon (icon)            
+        if not mediaObject:
+            with self.state:
+                self.pause = True
+            self.mediaObjects = True
+            
+            icon = QtGui.QIcon ()
+            icon.addPixmap (QtGui.QPixmap (os.path.join(ICON_PATH, 'play.png')), 
+                            QtGui.QIcon.Normal, 
+                            QtGui.QIcon.Off)                   
+            self.action_play.setIcon (icon) 
+
+    def startPlay(self):
+        startFrame = self.slider.value()    
+        endFrame = self.slider.maximum()
+        for index in range(startFrame, endFrame, 1):
+            with self.state:
+                if self.pause:
+                    self.state.wait()
+            self.slider.setValue(index+1)
+            time.sleep(1.00/self.fps)
     
-            self.slider.setProperty ('value', loop+1)
-            time.sleep (1.00/fps)
+    def stop(self):
+        try:
+            with self.state:
+                self.pause = True
+            self.mediaObjects = True
+            icon = QtGui.QIcon ()
+            icon.addPixmap (QtGui.QPixmap (os.path.join(ICON_PATH, 'play.png')), 
+                            QtGui.QIcon.Normal, 
+                            QtGui.QIcon.Off)                   
+            self.action_play.setIcon (icon)                   
+        except Exception as result:
+            warnings.warn(str(result), Warning)
         
-    def playSequence (self, files, play) :
-        fps = 25.00
-    
-        fLoop         = 1
-        for eachFile in files :
-           
-            self.button_preview.setIcon (QtGui.QIcon (os.path.join(self.footagePath, eachFile)))
-            self.button_preview.setIconSize (QtCore.QSize(self.resolution[0], self.resolution[1]))
-            self.button_preview.setMinimumSize (QtCore.QSize(self.resolution[0]+2, self.resolution[1]+2))               
-    
-            if play==1 :
-                time.sleep (1.00/fps)
-            fLoop+=1
-    
-        return 'Done'
-        
-        
-   
-        
-          
-        
+    def playSequence(self, files, play):
+        for index in range (len(files)):
+            self.button_preview.setIcon(QtGui.QIcon(os.path.join(self.footagePath, files[index])))
+            self.button_preview.setIconSize(QtCore.QSize(self.resolution[0], self.resolution[1]))
+            self.button_preview.setMinimumSize(QtCore.QSize(self.resolution[0]+2, self.resolution[1]+2))               
+            if not play:
+                continue
+            time.sleep(1.00/self.fps)    
         
     def getFiles(self):
         images = os.listdir(path=self.footagePath)
-        images.sort()
-        return images
+        filtImages = []
+        for each in images:
+            if not each.endswith(self.extension):
+                continue
+            filtImages.append(each)
+        filtImages.sort()
+        return filtImages
 
-    def playAndPauseAction_ (self, mode) :
-        icon     = QtGui.QIcon ()
-        icon.addPixmap (QtGui.QPixmap('path'), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.button_play.setIcon (icon)
-        self.button_play.setIconSize (QtCore.QSize(120,120))
-        self.button_play.setToolTip (mode) 
- 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    ex = Player()
+    footagePath = 'E:/Temp/old/Gallary_01'
+    ex = Player(path=footagePath)
     ex.show()
     sys.exit(app.exec_())
+
+#End########################################################################
